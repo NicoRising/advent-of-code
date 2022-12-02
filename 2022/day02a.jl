9d5e03698f47f0351273cb06bd0e5ecca13dd7d0
Nicholas Rising <nicholas.rising@gmail.com>
2022-12-02 12:53:38 +0000

    Make day 2 part a much nicer

--- 2022/day02a.jl
+++ 2022/day02a.jl
@@ -3,35 +3,13 @@ function main()
     score = 0
 
     for line in lines
-        if line[1] == 'A'
-            if line[3] == 'Y'
-                score += 6
-            elseif line[3] == 'X'
-                score += 3
-            end
-        elseif line[1] == 'B'
-            if line[3] == 'Z'
-                score += 6
-            elseif line[3] == 'Y'
-                score += 3
-            end
-        elseif line[1] == 'C'
-            if line[3] == 'X'
-                score += 6
-            elseif line[3] == 'Z'
-                score += 3
-            end
-        end
+        my_move = convert(Int8, line[3] - 88)
+        opponent_move = convert(Int8, line[1] - 65)
 
-        if line[3] == 'X'
-            score += 1
-        elseif line[3] == 'Y'
-            score += 2
-        else
-            score += 3
-        end
+        score += my_move + 1
+        score += 3 * mod(my_move - opponent_move + 1, 3)
     end
-
+    
     print("$score\n")
 end
 
