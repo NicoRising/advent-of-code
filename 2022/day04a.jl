1476c739a3aec0d2c0a0abcd15acd0ea246d73b8
Nicholas Rising <nicholas.rising@gmail.com>
2022-12-04 21:07:29 +0000

    Solve day 4

---
+++ 2022/day04a.jl
@@ -0,0 +1,19 @@
+function main()
+    pairs = readlines(open("input.txt"))
+    total = 0
+
+    for pair in pairs
+        nums = [parse(Int, num.match) for num in eachmatch(r"\d+", pair)]
+        
+        range1 = nums[1]:nums[2]
+        range2 = nums[3]:nums[4]
+
+        if issubset(range1, range2) || issubset(range2, range1)
+            total += 1
+        end
+    end
+
+    print("$total\n")
+end
+
+main()
