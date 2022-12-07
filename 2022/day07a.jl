abdc37ee336709a261ffdb6e58609bb2725bc7c4
Nicholas Rising <nicholas.rising@gmail.com>
2022-12-07 15:57:29 +0000

    Improve day 7 solution with structs

--- 2022/day07a.jl
+++ 2022/day07a.jl
@@ -1,6 +1,25 @@
-function size_of(dir)::Int
-    size = sum([file[2]::Int for file in dir[4]])
-    size += sum([size_of(child) for child in dir[3]])
+struct File
+    name::String
+    size::Int
+
+    File(name::String, size::Int) = new(name, size)
+end
+
+struct Directory
+    name::String
+    parent::Union{Directory, Nothing}
+    children::Vector{Directory}
+    files::Vector{File}
+
+    Directory(name::AbstractString, parent::Union{Directory, Nothing}) = new(name, parent, [], [])
+end
+
+function size_of(dir::Directory)::Int
+    size = sum([file.size for file in dir.files])
+    
+    if !isempty(dir.children)
+        size += sum([size_of(child) for child in dir.children])
+    end
     
     return size
 end
@@ -8,55 +27,50 @@ end
 function main()
     terminal = read(open("input.txt"), String)[3:(end - 1)]
     
-    root = (["/", Nothing, [], []])
-
-    root[2] = root
+    root = Directory("/", nothing)
     working = root
 
-    dirs = []
+    dirs = [root]
 
     for in_out in split(terminal, "\n\$ ")
         in_out = split(in_out, '\n')
         command = in_out[1][1:2]
 
         if command == "cd"
-            dir = in_out[1][4:end]
+            dir_name = in_out[1][4:end]
             
-            if dir == "/"
+            if dir_name == "/"
                 working = root
-            elseif dir == ".."
-                working = working[2]
+            elseif dir_name == ".."
+                working = working.parent
             else
-                index = findfirst(child -> child[1] == dir, working[3])
-                working = working[3][index]
+                index = findfirst(child -> child.name == dir_name, working.children)
+                working = working.children[index]
             end
         elseif command == "ls"
             for line in in_out[2:end]
                 if length(line) >= 3 && line[1:3] == "dir"
-                    dir = line[5:end]
+                    dir_name = line[5:end]
 
-                    if isnothing(findfirst(child -> child[1] == dir, working[3]))
-                        new_dir = (dir, working, [], [])
-                        push!(working[3], new_dir)
+                    if isnothing(findfirst(child -> child.name == dir_name, working.children))
+                        new_dir = Directory(dir_name, working)
+
+                        push!(working.children, new_dir)
                         push!(dirs, new_dir)
                     end
                 else
-                    size, file = split(line, " ")
+                    size, file_name::String = split(line, " ")
+                    size = parse(Int, size)
 
-                    if isnothing(findfirst(child -> child[1] == file, working[4]))
-                        push!(working[4], (file, parse(Int, size)))
+                    if isnothing(findfirst(child -> child.name == file_name, working.files))
+                        push!(working.files, File(file_name, size))
                     end
                 end
             end
         end
     end
 
-    sizes = []
-
-    for dir in dirs
-        push!(sizes, size_of(dir))
-    end
-
+    sizes = [size_of(dir) for dir in dirs]
     println(sum([size for size in sizes if size <= 100_000]))
 end
 
--- 2022/day07b.jl
+++ 2022/day07b.jl
@@ -1,6 +1,25 @@
-function size_of(dir)::Int
-    size = sum([file[2]::Int for file in dir[4]])
-    size += sum([size_of(child) for child in dir[3]])
+struct File
+    name::String
+    size::Int
+
+    File(name::String, size::Int) = new(name, size)
+end
+
+struct Directory
+    name::String
+    parent::Union{Directory, Nothing}
+    children::Vector{Directory}
+    files::Vector{File}
+
+    Directory(name::AbstractString, parent::Union{Directory, Nothing}) = new(name, parent, [], [])
+end
+
+function size_of(dir::Directory)::Int
+    size = sum([file.size for file in dir.files])
+    
+    if !isempty(dir.children)
+        size += sum([size_of(child) for child in dir.children])
+    end
     
     return size
 end
@@ -8,10 +27,9 @@ end
 function main()
     terminal = read(open("input.txt"), String)[3:(end - 1)]
     
-    root = ["/", nothing, [], []]
-    root[2] = root
-
+    root = Directory("/", nothing)
     working = root
+
     dirs = [root]
 
     for in_out in split(terminal, "\n\$ ")
@@ -19,42 +37,40 @@ function main()
         command = in_out[1][1:2]
 
         if command == "cd"
-            dir = in_out[1][4:end]
+            dir_name = in_out[1][4:end]
             
-            if dir == "/"
+            if dir_name == "/"
                 working = root
-            elseif dir == ".."
-                working = working[2]
+            elseif dir_name == ".."
+                working = working.parent
             else
-                index = findfirst(child -> child[1] == dir, working[3])
-                working = working[3][index]
+                index = findfirst(child -> child.name == dir_name, working.children)
+                working = working.children[index]
             end
         elseif command == "ls"
             for line in in_out[2:end]
                 if length(line) >= 3 && line[1:3] == "dir"
-                    dir = line[5:end]
+                    dir_name = line[5:end]
 
-                    if isnothing(findfirst(child -> child[1] == dir, working[3]))
-                        new_dir = [dir, working, [], []]
-                        push!(working[3], new_dir)
+                    if isnothing(findfirst(child -> child.name == dir_name, working.children))
+                        new_dir = Directory(dir_name, working)
+
+                        push!(working.children, new_dir)
                         push!(dirs, new_dir)
                     end
                 else
-                    size, file = split(line, " ")
+                    size, file_name::String = split(line, " ")
+                    size = parse(Int, size)
 
-                    if isnothing(findfirst(child -> child[1] == file, working[4]))
-                        push!(working[4], (file, parse(Int, size)))
+                    if isnothing(findfirst(child -> child.name == file_name, working.files))
+                        push!(working.files, File(file_name, size))
                     end
                 end
             end
         end
     end
 
-    sizes = []
-
-    for dir in dirs
-        push!(sizes, size_of(dir))
-    end
+    sizes = [size_of(dir) for dir in dirs]
 
     free_up = sizes[1] - (70_000_000 - 30_000_000)
     min = sizes[1]
