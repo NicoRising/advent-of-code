c80e432d37df6aa4b929bfb696e3c4a5abb6e884
Nico Rising <nico@nicorising.com>
2023-10-18 15:44:57 -0400

    Clean up 2015 day 1 and update .gitignore to ignore binaries

--- 2015/day01a.c
+++ 2015/day01a.c
@@ -2,11 +2,14 @@
 
 void main() {
     FILE *input = fopen("input.txt", "r");
+
     int floor = 0;
     int current = fgetc(input);
-    while (current != EOF) {
-        floor += (current == 40) ? 1 : -1;
+
+    while (current != '\n') {
+        floor += current == 40 ? 1 : -1;
         current = fgetc(input);
     }
+
     printf("%d\n", floor);
 }
--- 2015/day01b.c
+++ 2015/day01b.c
@@ -2,13 +2,17 @@
 
 void main() {
     FILE *input = fopen("input.txt", "r");
+
     int floor = 0;
     int step = 0;
     int current = fgetc(input);
+
     while (floor >= 0) {
         floor += (current == 40) ? 1 : -1;
         step++;
+
         current = fgetc(input);
     }
+
     printf("%d\n", step);
 }
