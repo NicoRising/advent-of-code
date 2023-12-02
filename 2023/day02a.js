3eb2e3bd7f2848c82aaa13691842284cb5097f4e
Nico Rising <nico@nicorising.com>
2023-12-02 00:38:21 -0500

    Clean up code a bit

--- 2023/day02a.js
+++ 2023/day02a.js
@@ -7,34 +7,27 @@ fs.readFile("input.txt", "utf8", (err, text) => {
         const lines = text.split(/\n/).slice(0, -1);
         let sum = 0;
 
+        lineLoop:
         for (const [idx, line] of lines.entries()) {
             draws = line.substring(line.indexOf(":") + 2).split("; ");
-            let impossible = false;
             
             for (const draw of draws) {
-                const matches = draw.split(", ");
+                const dice = draw.split(", ");
 
-                for (let match of matches) {
-                    match = match.split(" ")
-                    const count = Number(match[0]);
-                    const color = match[1];
+                for (const die of dice) {
+                    let [count, color] = die.split(" ");
+                    count = Number(count);
 
-                    if (color == "red" && count > 12) {
-                        impossible = true
-                        break
-                    } else if (color == "green" && count > 13) {
-                        impossible = true
-                        break
-                    } else if (color == "blue" && count > 14) {
-                        impossible = true
-                        break
+                    if (color == "red"   && count > 12 ||
+                        color == "green" && count > 13 ||
+                        color == "blue"  && count > 14) {
+                        
+                        continue lineLoop;
                     }
                 }
-            }   
-
-            if (!impossible) {
-                sum += idx + 1;
             }
+
+            sum += idx + 1;
         }
 
         console.log(sum);
--- 2023/day02b.js
+++ 2023/day02b.js
@@ -4,36 +4,34 @@ fs.readFile("input.txt", "utf8", (err, text) => {
     if (err) {
         console.error(err);
     } else {
-        const lines = text.split(/\n/).slice(0, -1);
+        const lines = text.split("\n").slice(0, -1);
         let sum = 0;
 
-        for (const [idx, line] of lines.entries()) {
+        for (const line of lines) {
             draws = line.substring(line.indexOf(":") + 2).split("; ");
-            let impossible = false;
+
             let maxRed = 0;
             let maxGreen = 0;
-            let maxBlue = 0
+            let maxBlue = 0;
             
             for (const draw of draws) {
-                const matches = draw.split(", ");
+                const dice = draw.split(", ");
 
-                for (let match of matches) {
-                    match = match.split(" ")
-                    const count = Number(match[0]);
-                    const color = match[1];
+                for (const die of dice) {
+                    let [count, color] = die.split(" ");
+                    count = Number(count);
 
                     if (color == "red" && count > maxRed) {
                         maxRed = count;
                     } else if (color == "green" && count > maxGreen) {
                         maxGreen = count;
                     } else if (color == "blue" && count > maxBlue) {
-                        maxBlue = count
+                        maxBlue = count;
                     }
                 }
             }   
 
-            sum += maxRed * maxGreen * maxBlue
-
+            sum += maxRed * maxGreen * maxBlue;
         }
 
         console.log(sum);
