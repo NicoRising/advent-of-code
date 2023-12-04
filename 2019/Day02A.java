1f5c2d9e181cce20a9e3597135b6c8858e4d4333
Nico Rising <nico@nicorising.com>
2023-12-04 16:34:51 -0500

    Clean up day 2 2019 code

--- 2019/Day02A.java
+++ 2019/Day02A.java
@@ -6,27 +6,46 @@ public class Day02A {
     public static void main(String[] args) throws FileNotFoundException {
         String[] input = new Scanner(new File("input.txt")).next().split(",");
         int[] program = new int[input.length];
-        for(int index = 0; index < input.length; index++) {
+
+        for (int index = 0; index < input.length; index++) {
             program[index] = Integer.parseInt(input[index]);
         }
+
         program[1] = 12;
         program[2] = 2;
-        int index = 0;
+
+        int idx = 0;
+        int idx_a, idx_b, idx_c;
         boolean halt = false;
+
         while (!halt) {
-            switch (program[index]) {
+            switch (program[idx]) {
                 case 1:
-                    program[program[index + 3]] = program[program[index + 1]] + program[program[index + 2]];
-                    index += 4;
+                    idx_a = program[idx + 1];
+                    idx_b = program[idx + 2];
+                    idx_c = program[idx + 3];
+
+                    program[idx_c] = program[idx_a] + program[idx_b];
+
+                    idx += 4;
                     break;
                 case 2:
-                    program[program[index + 3]] = program[program[index + 1]] * program[program[index + 2]];
-                    index += 4;
+                    idx_a = program[idx + 1];
+                    idx_b = program[idx + 2];
+                    idx_c = program[idx + 3];
+
+                    program[idx_c] = program[idx_a] * program[idx_b];
+
+                    idx += 4;
+                    break;
+                case 99:
+                    halt = true;
                     break;
                 default:
-                    halt = true;
+                    throw new RuntimeException("Invalid opcode: " + program[idx]);
             }
         }
+
         System.out.println(program[0]);
     }
 }
--- 2019/Day02B.java
+++ 2019/Day02B.java
@@ -4,38 +4,57 @@ import java.util.Scanner;
 
 public class Day02B {
     public static void main(String[] args) throws FileNotFoundException {
-        String[] input = new Scanner(new File("Input.txt")).next().split(",");
-        int[] program = new int[input.length];
+        String[] input = new Scanner(new File("input.txt")).next().split(",");
+        int[] originalProgram = new int[input.length];
+
         for (int index = 0; index < input.length; index++) {
-            program[index] = Integer.parseInt(input[index]);
+            originalProgram[index] = Integer.parseInt(input[index]);
         }
-        int out = 0;
-        for (int noun = 0; noun < 100 && out == 0; noun++) {
-            for (int verb = 0; verb < 100 && out == 0; verb++) {
-                int[] copy = program.clone();
-                copy[1] = noun;
-                copy[2] = verb;
-                int index = 0;
+
+        for (int noun = 0; noun <= 99; noun++) {
+            for (int verb = 0; verb <= 99; verb++) {
+                int[] program = originalProgram.clone();
+
+                program[1] = noun;
+                program[2] = verb;
+
+                int idx = 0;
+                int idx_a, idx_b, idx_c;
                 boolean halt = false;
+
                 while (!halt) {
-                    switch (copy[index]) {
+                    switch (program[idx]) {
                         case 1:
-                            copy[copy[index + 3]] = copy[copy[index + 1]] + copy[copy[index + 2]];
-                            index += 4;
+                            idx_a = program[idx + 1];
+                            idx_b = program[idx + 2];
+                            idx_c = program[idx + 3];
+
+                            program[idx_c] = program[idx_a] + program[idx_b];
+
+                            idx += 4;
                             break;
                         case 2:
-                            copy[copy[index + 3]] = copy[copy[index + 1]] * copy[copy[index + 2]];
-                            index += 4;
+                            idx_a = program[idx + 1];
+                            idx_b = program[idx + 2];
+                            idx_c = program[idx + 3];
+
+                            program[idx_c] = program[idx_a] * program[idx_b];
+
+                            idx += 4;
+                            break;
+                        case 99:
+                            halt = true;
                             break;
                         default:
-                            halt = true;
+                            throw new RuntimeException("Invalid opcode: " + program[idx]);
                     }
                 }
-                if(copy[0] == 19690720) {
-                    out = noun * 100 + verb;
+
+                if (program[0] == 19690720) {
+                    System.out.println(noun * 100 + verb);
+                    return;
                 }
             }
         }
-        System.out.println(out);
     }
 }
