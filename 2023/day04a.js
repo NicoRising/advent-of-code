da7d145bce691d9fd80f6a1221a314bbd5adc0f0
Nico Rising <nico@nicorising.com>
2023-12-04 00:37:47 -0500

    Clean up part a

--- 2023/day04a.js
+++ 2023/day04a.js
@@ -5,32 +5,20 @@ fs.readFile("input.txt", "utf8", (err, text) => {
         console.error(err);
     } else {
         const lines = text.split("\n").slice(0, -1);
-
         let sum = 0;
 
         for (const line of lines) {
-            let [yours, winning] = line.split("|");
+            let [yours, winning] = line.substring(line.indexOf(":")).split("|");
             yours = yours.match(/\d+/g);
-            yours = yours.slice(1);
-            yours = [...new Set(yours)];
             winning = winning.match(/\d+/g);
-            console.log(yours)
-            console.log(winning)
             
-            let score = 0;
+            let matches = 0;
 
             for (const num of yours) {
-                if (winning.includes(num)) {
-                    console.log(num);
-                    if (score == 0) {
-                        score++;
-                    } else {
-                        score *= 2;
-                    }
-                }
+                matches += winning.includes(num);
             }
 
-            sum += score;
+            sum += Math.floor(2 ** (matches - 1));
         }
 
         console.log(sum);
