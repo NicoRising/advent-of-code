7ff4c11e66396c7d934eef3964068069c92f9520
Nico Rising <nico@nicorising.com>
2023-12-07 21:18:39 -0500

    Clean up day 8 of 2019 code

--- 2019/Day08A.java
+++ 2019/Day08A.java
@@ -1,36 +1,32 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Day08A {
     public static void main(String[] args) throws FileNotFoundException {
         final int WIDTH = 25;
         final int HEIGHT = 6;
+
         String input = new Scanner(new File("input.txt")).next();
-        int[][][] layers = new int[input.length() / WIDTH / HEIGHT][HEIGHT][WIDTH];
-        int min = 0;
-        int minLayer = 0;
-        for (int layer = 0; layer < layers.length; layer++) {
-            int count = 0;
-            for (int row = 0; row < HEIGHT; row++) {
-                for (int col = 0; col < WIDTH; col++) {
-                layers[layer][row][col] = Integer.parseInt(input.charAt(layer * WIDTH * HEIGHT + row * WIDTH + col) + "");
-                count += layers[layer][row][col] == 0 ? 1 : 0;
+
+        int[] counts = {0, 0, 0};
+        int minZeros = WIDTH * HEIGHT + 1;
+        int product = 0;
+
+        for (int idx = 0; idx < input.length(); idx++) {
+            counts[input.charAt(idx) - '0']++;
+
+            if ((idx + 1) % (WIDTH * HEIGHT) == 0) {
+                if (counts[0] < minZeros) {
+                    minZeros = counts[0];
+                    product = counts[1] * counts[2];
                 }
-            }
-            if (count < min||min == 0) {
-                min = count;
-                minLayer = layer;
-            }
-        }
-        int oneCount = 0;
-        int twoCount = 0;
-        for (int row = 0; row < HEIGHT; row++) {
-            for (int col = 0; col < WIDTH; col++) {
-                oneCount += layers[minLayer][row][col] == 1 ? 1 : 0;
-                twoCount += layers[minLayer][row][col] == 2 ? 1 : 0;
+
+                counts[0] = 0;
+                counts[1] = 0;
+                counts[2] = 0;
             }
         }
-        System.out.println(oneCount * twoCount);
+
+        System.out.println(product);
     }
 }
